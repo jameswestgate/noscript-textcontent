546e34128d61b9c64a58f55b189aaeddbdfb58fa
James Westgate <jwestgate@as0564.local>
2012-07-11 10:23:07 +0100

    Improved polyfill logic
    Make ajax request synchronous and remove dom ready code. Reduced file
    size.

--- src/noscript.js
+++ src/noscript.js
@@ -5,63 +5,29 @@
 
 (function() {
 
-	var retry = 0;
+	var div = document.createElement("div");
+	div.innerHTML = "<noscript><span></span></noscript>";
+	
+	var elements = div.getElementsByTagName("noscript");
+	if (elements.length && elements[0].textContent) return; //No polyfill required
 
-	var t = setTimeout(function() {
+	//Re-request the page via ajax
+	//Patch XMLHttpRequest if required
+	var xmlhttp = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP'); //For IE6 and below only, good enough for our purposes
+	xmlhttp.open("GET", window.location, false); //syncronous call since we need to block
+	xmlhttp.send();
 
-		if (document.body) {
-			var div = document.createElement("div");
-			div.innerHTML = "<noscript><span></span></noscript>";
-			
-			var elements = div.getElementsByTagName("noscript");
+	//Process the response
+	var tags = document.getElementsByTagName("noscript");
+	var regex = /<noscript>(.*?)<\/noscript>/igm; 
+	var results, response = xmlhttp.responseText;
+	var i=0, len=tags.length;
 
-			if (elements.length && elements[0].textContent) return; //No polyfill required
-
-			//Re-request the page via ajax
-			patchHttpRequest();
-			var xmlhttp = new XMLHttpRequest();
-
-			xmlhttp.onreadystatechange = function() {
-				
-				if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-					
-					var regex = /<noscript[^>]*?>(.*?)<\/noscript>/igm;
-					var tags = document.getElementsByTagName("noscript");
-					var results = [], response = xmlhttp.responseText;
-					var i=0, len=tags.length;
-
-					while (i < len && results) {
-						results = regex.exec(response);
-						tags[i].textContent = results[1];
-						i++;
-					}
-				}
-			}
-
-			xmlhttp.open("GET", window.location, true);
-			xmlhttp.send();
-		}
-		else {
-			retry ++;
-			if (retry > 50) return; //give up after > 1 second
-			t = setTimeout(arguments.callee, 20); //Retry in 20 milliseconds
-		}
-
-	}, 0);
-
-	function patchHttpRequest() {
-
-		//Patch XMLHttpRequest if required
-		if (typeof window.XMLHttpRequest === 'undefined' && typeof window.ActiveXObject === 'function') {
- 
-		    window.XMLHttpRequest = function() {
-		        try { return new ActiveXObject('Msxml2.XMLHTTP.6.0'); } catch(e) {}
-		        try { return new ActiveXObject('Msxml2.XMLHTTP.3.0'); } catch(e) {}
-		        return new ActiveXObject('Microsoft.XMLHTTP');
-		    };
-		}
+	while (i < len) {
+		results = regex.exec(response);
+		if (results) tags[i].textContent = results[1];
+		i++;
 	}
-
 })();
 
 
